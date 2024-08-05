c9e5bc385f6dde345126217b555e4b2e341768f1
kimju-hee <lsj240606@naver.com>
2024-08-05 10:31:19 +0900

    회전초밥 풀이

---
+++ Juhee_Python/06_TwoPointer/Baekjoon_2531.py
@@ -0,0 +1,18 @@
+N, d, k, c = map(int, input().split())
+A = []
+
+for i in range(N):
+    a = int(input())
+    A.append(a)
+count = []
+
+A += A[:-1] # 원형으로 만들기
+for i in range(N):
+    a = [c]
+    a += A[i:i+k]
+    a = set(a)
+    count.append(len(a))
+    a =[c]
+
+
+print(max(count))
