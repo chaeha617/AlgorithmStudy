d40fe84a51cbc9c9b4959be9b6dc5ee8a0c076f6
kimju-hee <lsj240606@naver.com>
2024-08-30 15:17:25 +0900

    계단오르기

--- Juhee_Python/ETC/Baekjoon_2579.py
+++ Juhee_Python/ETC/Baekjoon_2579.py
@@ -1,16 +1,15 @@
-
 N = int(input())
-A = [0] * 301
+stair = [0] * 301
 for i in range(1, N+1):
     a = int(input())
-    A[i] = a
+    stair[i] = a
 
-dp = [0] * 301
-dp[1] = A[1]
-dp[2] = A[0] + A[1]
-dp[3] = max(A[1] +A[3], A[2]+A[3])
+dp = [0] * (N+1)
+
+dp[1] = stair[1]
+dp[2] = stair[2] + stair[1]
+dp[3] = max(stair[1]+stair[3], stair[2]+stair[3])
 
 for i in range(4, N+1):
-    dp[i] = max(dp[i-3]+A[i-1]+A[i], dp[i-2]+A[i])
-
+    dp[i] = max(dp[i-3]+stair[i-1]+stair[i], dp[i-2]+stair[i])
 print(dp[N])
