df8be62cfb8ff86405b091275e9cd9aad3fe27e1
kimch <rlacogk6577@daum.net>
2024-09-02 10:26:01 +0900

    폴더 수정 및 풀이 커밋

--- ChaeHa_Python/10_DPRevenge/beakjoon_14501.py
+++ ChaeHa_Python/10_DPRevenge/beakjoon_14501.py
@@ -1,10 +1,17 @@
 N = int(input())
 
-counsel = []
-dp = [0]* N
+#배열 인덱스 1부터 시작하기 위한 초기화
+counsel = [[0,0]]
+dp = [0] * (N+1)
 
 for _ in range(N):
     counsel.append(list(map(int, input().split(" "))))
 
-dp[1] = 1
-print(dp)
+for i in range(1,N+1):
+    day, val = counsel[i]
+    val += max(dp[:i])
+    day = i + day -1
+    if day <= N:
+        dp[day] = max(dp[day],val)
+
+print(max(dp))
--- ChaeHa_Python/10_DPRevenge/beakjoon_17070.py
+++ ChaeHa_Python/10_DPRevenge/beakjoon_17070.py
@@ -9,5 +9,27 @@ field = []
 for _ in range(N):
     field.append(list(map(int,input().split(" "))))
 
-dp = []
+#[가로, 세로, 대각선]
+dp = [[[0,0,0] for __ in range(N)]  for _ in range(N)]
+dp[0][1][0] = 1
 
+for i in range(N):
+    for j in range(N):
+        if i == 0 and j == 1:
+            continue
+        for direct in range(3):
+            if field[i][j] == 0:
+                pipe = 0
+                if i - 1 > -1 and j - 1 > -1:  # 대각선 존재
+                    pipe += dp[i-1][j-1][2]
+                # 세로, 대각선의 값 구할때
+                if direct != 0 and i - 1 > -1:
+                    pipe += dp[i-1][j][1]
+                # 가로, 대각선 값 구할때
+                if direct != 1 and j - 1 > -1:
+                    pipe += dp[i][j-1][0]
+                dp[i][j][direct] = pipe
+
+for li in dp:
+    print(li)
+print(max(dp[N-1][N-1]))
