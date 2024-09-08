3760c74afd3a7aa0c80562712918879217ce80b1
김주희 <lsj240606@naver.com>
2024-09-08 14:13:27 +0900

    연산자 끼워넣기

---
+++ Juhee_Python/11_삼성 SW 역량 테스트 기출 문제/Baekjoon_14888.py
@@ -0,0 +1,31 @@
+# 백트래킹으로 풀어보기
+
+N = int(input())
+M = list(map(int, input().split()))
+# 순서대로 +,-,*,//
+add, sub, mul, div = map(int, input().split())
+
+
+def dfs(n, sm, add, sub, mul, div): \
+        global mn, mx
+# 종료 조건(정답처리)
+if n == N:
+    mn = min(mn, sm)
+    mx = max(mx, sm)
+    return
+
+# 연산자 개수 남았을 경우에만 호출
+if add > 0:
+    dfs(n + 1, sm + M[n], add - 1, sub, mul, div)
+if sub > 0:
+    dfs(n + 1, sm - M[n], add, sub - 1, mul, div)
+if mul > 0:
+    dfs(n + 1, sm * M[n], add, sub, mul - 1, div)
+if div > 0:
+    dfs(n + 1, int(sm / M[n]), add, sub, mul, div - 1)
+
+mx = int(-1e9)
+mn = int(1e9)
+
+dfs(1, M[0], add, sub, mul, div)
+print(mx, mn)
