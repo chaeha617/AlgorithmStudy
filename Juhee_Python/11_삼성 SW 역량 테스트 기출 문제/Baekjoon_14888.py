cc014bb81d826af8d099501a328c5e4cbb871f97
kimju-hee <lsj240606@naver.com>
2024-09-09 12:46:53 +0900

    수정

--- Juhee_Python/11_삼성 SW 역량 테스트 기출 문제/Baekjoon_14888.py
+++ Juhee_Python/11_삼성 SW 역량 테스트 기출 문제/Baekjoon_14888.py
@@ -6,23 +6,23 @@ M = list(map(int, input().split()))
 add, sub, mul, div = map(int, input().split())
 
 
-def dfs(n, sm, add, sub, mul, div): \
+def dfs(n, sm, add, sub, mul, div):
         global mn, mx
-# 종료 조건(정답처리)
-if n == N:
-    mn = min(mn, sm)
-    mx = max(mx, sm)
-    return
+        # 종료 조건(정답처리)
+        if n == N:
+            mn = min(mn, sm)
+            mx = max(mx, sm)
+            return
 
-# 연산자 개수 남았을 경우에만 호출
-if add > 0:
-    dfs(n + 1, sm + M[n], add - 1, sub, mul, div)
-if sub > 0:
-    dfs(n + 1, sm - M[n], add, sub - 1, mul, div)
-if mul > 0:
-    dfs(n + 1, sm * M[n], add, sub, mul - 1, div)
-if div > 0:
-    dfs(n + 1, int(sm / M[n]), add, sub, mul, div - 1)
+        # 연산자 개수 남았을 경우에만 호출
+        if add > 0:
+            dfs(n + 1, sm + M[n], add - 1, sub, mul, div)
+        if sub > 0:
+            dfs(n + 1, sm - M[n], add, sub - 1, mul, div)
+        if mul > 0:
+            dfs(n + 1, sm * M[n], add, sub, mul - 1, div)
+        if div > 0:
+            dfs(n + 1, int(sm / M[n]), add, sub, mul, div - 1)
 
 mx = int(-1e9)
 mn = int(1e9)
