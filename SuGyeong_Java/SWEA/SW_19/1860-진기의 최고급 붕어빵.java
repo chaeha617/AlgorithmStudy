22d2c1c68f85dd597aa1621c995b2cd4e1660809
minisudal <sukunz1228@naver.com>
2024-11-08 02:56:44 +0900

    19주차 주석으로 풀이설명

--- SuGyeong_Java/SWEA/SW_19/1860-진기의 최고급 붕어빵.java
+++ SuGyeong_Java/SWEA/SW_19/1860-진기의 최고급 붕어빵.java
@@ -3,22 +3,25 @@ import java.util.*;
 
 public class Main {
     public static int possible(int[] arr, int n, int m, int k) {
-        if(arr[0] < m) {
-            return 0;
+        if(arr[0] < m) { // 가장 빨리 도착하는 사람이 붕어빵을 만들기 전이라면 실패
+            return 0; // 실패 시 0 리턴
         } else {
-            for(int i = 0; i < n; i++){
-                int num = arr[i] / m;
-                int num2 = num * k;
+            for(int i = 0; i < n; i++){ // 모든 사람의 도착 시간에 대해 확인
+                int num = arr[i] / m; // 현재 시간까지 만든 붕어빵의 세트 수 (도착 시간 / 붕어빵 제작 시간)
+                int num2 = num * k; // 도착 시간까지 만든 총 붕어빵 수 (세트 수 * 세트당 붕어빵 수)
+
+                // 도착한 사람의 순서보다 붕어빵이 부족하면 실패
                 if(num2 < i + 1) {
                     return 0;
                 }
             }
-            return 1;
+            return 1; // 성공 시 1 리턴
         }
     }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine()); // 테스트 케이스 수 입력
 
         for(int t = 0; t < T; t++){
             StringTokenizer tr = new StringTokenizer(br.readLine());
@@ -26,15 +29,18 @@ public class Main {
             int m = Integer.parseInt(tr.nextToken());
             int k = Integer.parseInt(tr.nextToken());
 
-            int[] arr = new int[n];
+            int[] arr = new int[n]; // 사람들이 도착하는 시간 저장 배열
             tr = new StringTokenizer(br.readLine());
             for(int i = 0; i < n; i++){
                 arr[i] = Integer.parseInt(tr.nextToken());
-            } Arrays.sort(arr);
+            }
+            Arrays.sort(arr); // 도착 시간을 오름차순 정렬하여 가장 빨리 도착하는 사람부터 확인
 
-            if(possible(arr, n, m, k) == 0){
+            if(possible(arr, n, m, k) == 0) { // 실패하면 "Impossible" 출력
                 System.out.println("#" + (t + 1) + " " + "Impossible");
-            }else System.out.println("#" + (t + 1) + " " + "Possible");
+            } else { // 성공하면 "Possible" 출력
+                System.out.println("#" + (t + 1) + " " + "Possible");
+            }
         }
     }
 }
--- SuGyeong_Java/SWEA/SW_19/1873-상호의 배틀필드.java
+++ SuGyeong_Java/SWEA/SW_19/1873-상호의 배틀필드.java
@@ -10,11 +10,11 @@ public class Main {
 
         if (str.charAt(count) == 'U') {
             count++;
-            field[x][y] = '^';
-            if (x - 1 >= 0 && field[x - 1][y] == '.') {
-                field[x][y] = '.';
-                field[x - 1][y] = '^';
-                move(str, x - 1, y, count);
+            field[x][y] = '^'; // 문자 바꿔주고
+            if (x - 1 >= 0 && field[x - 1][y] == '.') { // 탐색할 위치가 범위를 벗어나지 않으면서 평지('.')일 때
+                field[x][y] = '.'; // 현재위치 문자를 평지로 바꾸고
+                field[x - 1][y] = '^'; // 탐색위치의 문자를 전차로 바꿔주기
+                move(str, x - 1, y, count); // 다음 전차에서 위치 탐색
             } else {
                 move(str, x, y, count);
             }
@@ -50,11 +50,11 @@ public class Main {
             }
         } else if (str.charAt(count) == 'S') {
             count++;
-            if (field[x][y] == '<') {
-                for (int i = y - 1; i >= 0; i--) {
-                    if (field[x][i] == '#') break;
-                    else if (field[x][i] == '*') {
-                        field[x][i] = '.';
+            if (field[x][y] == '<') { // 전차가 왼쪽으로 볼때
+                for (int i = y - 1; i >= 0; i--) { // 왼쪽으로 포탑 쏘기
+                    if (field[x][i] == '#') break; // 강철 벽이면 아무것도 하지않고 break;
+                    else if (field[x][i] == '*') { // 벽돌 벽이면
+                        field[x][i] = '.'; // 벽돌 위치의 문자를 평지로 바꿔주고 break;
                         break;
                     }
                 }
@@ -83,7 +83,7 @@ public class Main {
                     }
                 }
             }
-            move(str, x, y, count);
+            move(str, x, y, count); // 포탑 쏘고 다음 탐색
         }
     }
 
@@ -102,7 +102,7 @@ public class Main {
                 String str = br.readLine();
                 for (int j = 0; j < w; j++) {
                     field[i][j] = str.charAt(j);
-                    if (field[i][j] == '<' || field[i][j] == 'v' || field[i][j] == '^' || field[i][j] == '>') {
+                    if (field[i][j] == '<' || field[i][j] == 'v' || field[i][j] == '^' || field[i][j] == '>') { // 전차의 위치를 x,y에 저장
                         x = i;
                         y = j;
                     }
@@ -110,11 +110,11 @@ public class Main {
             }
 
             n = Integer.parseInt(br.readLine());
-            String str = br.readLine();
+            String str = br.readLine(); // 입력 종류 str
 
             int count = 0;
             move(str, x, y, count);
-            System.out.print("#" + (t + 1) + " ");
+            System.out.print("#" + (t + 1) + " "); // 출력
             for (int i = 0; i < h; i++) {
                 for(int j = 0; j < w; j++){
                     System.out.print(field[i][j]);
