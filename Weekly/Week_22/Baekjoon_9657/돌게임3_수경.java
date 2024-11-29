580d4b0ccff0397dcf2cc78f28bcb320166e940b
minisudal <sukunz1228@naver.com>
2024-11-29 10:46:24 +0900

    22주차 돌게임3 풀이

---
+++ Weekly/Week_22/Baekjoon_9657/돌게임3_수경.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        boolean[] dp = new boolean[n + 1];
+
+        if (n >= 1) dp[1] = true;
+        if (n >= 2) dp[2] = false;
+        if (n >= 3) dp[3] = true;
+        if (n >= 4) dp[4] = true;
+
+        for(int i = 5; i <= n; i++){
+            dp[i] = !dp[i - 1] || !dp[i - 3] || !dp[i - 4];
+        }
+
+        if (dp[n]) System.out.println("SK");
+        else System.out.println("CY");
+    }
+}
