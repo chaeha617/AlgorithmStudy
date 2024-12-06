7166b8b58c602aa35bfddc09602a18e9cb796a1a
kimch <rlacogk6577@daum.net>
2024-12-06 12:17:37 +0900

    신규아이디추천 문제 풀이

---
+++ Weekly/Week_23/Programmers/신규아이디추천_채하.py
@@ -0,0 +1,22 @@
+import re
+
+def solution(new_id):
+    answer = ""
+    new_id = new_id.lower()
+    for ch in new_id:
+        if re.fullmatch('[a-z0-9\-_.]', ch):
+            if ch == "." and (answer == "" or answer[-1] == "."):
+                continue
+            answer += ch
+    if answer and answer[-1] == ".":
+        answer = answer[:-1]
+    if not answer:
+        answer = "a"
+    if len(answer) > 15:
+        answer = answer[:15]
+        if answer[-1] == ".":
+            answer = answer[:-1]
+    while len(answer) < 3:
+        answer += answer[-1]
+
+    return answer
