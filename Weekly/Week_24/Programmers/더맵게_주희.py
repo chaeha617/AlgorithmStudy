ef9a4e3bfefaed50a268d7fa5304f0426689a1b2
kimju-hee <lsj240606@naver.com>
2024-12-06 14:09:58 +0900

    더맵게

--- Weekly/Week_24/Programmers/더맵게_주희.py
+++ Weekly/Week_24/Programmers/더맵게_주희.py
@@ -1,3 +1,19 @@
+import heapq
 def solution(scoville, K):
-    answer = 0
-    return answer
+    task = []
+    for i in scoville:
+        heapq.heappush(task, i)
+    count = 0
+
+    while task[0] < K:
+        if len(task) < 2:
+            return -1
+        a = heapq.heappop(task)
+        b = heapq.heappop(task)
+        heapq.heappush(task, a+(b*2))
+        count += 1
+
+
+    return count
+
+solution([1, 2, 3, 9, 10, 12], 7)
