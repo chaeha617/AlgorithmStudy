ff1c3b6fba695a466d59f510e8ced0626912354c
kimch <rlacogk6577@daum.net>
2025-01-03 12:47:44 +0900

    27주차 문제 풀이

--- Weekly/Week_27/beakjoon_20125/쿠키의신체측정_채하.java
+++ Weekly/Week_27/beakjoon_20125/쿠키의신체측정_채하.java
@@ -4,10 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
 
-public class 쿠키의신체측청_채하 {
+public class 쿠키의신체측정_채하 {
 
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -41,17 +39,17 @@ public class 쿠키의신체측청_채하 {
                 waist++;
             }
 
-            leftLeg = 0;
-            rightLeg = 0;
+            leftLeg = 1;
+            rightLeg = 1;
             //다리길이 구하기
-            while (row.charAt(heart[1] - 1) == '*' || row.charAt(heart[1] + 1) == '*' && index < field.size()) {
+            while (index < field.size() - 1 && (row.charAt(heart[1] - 1) == '*' || row.charAt(heart[1] + 1) == '*')) {
+                row = field.get(++index);
                 if (row.charAt(heart[1] - 1) == '*') {
                     rightLeg++;
                 }
-                if (row.charAt(heart[1] - 1) == '*') {
+                if (row.charAt(heart[1] + 1) == '*') {
                     leftLeg++;
                 }
-                row = field.get(++index);
             }
 
 
@@ -59,8 +57,8 @@ public class 쿠키의신체측청_채하 {
             throw new RuntimeException(e);
         }
 
-        System.out.println(heart[0] + " " + heart[1]);
-        System.out.println(rightArm + " " + rightArm + " " + waist + " " + leftLeg  + " " + rightLeg);
+        System.out.println((heart[0]+1) + " " + (heart[1]+1));
+        System.out.println(rightArm + " " + leftArm + " " + (waist - 1) + " " + rightLeg + " " + leftLeg);
     }
 
 
