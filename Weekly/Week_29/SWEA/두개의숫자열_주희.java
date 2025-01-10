46b3cb8fdd16d7d10d645a6ae9ded1775834be85
kimju-hee <lsj240606@naver.com>
2025-01-10 18:13:43 +0900

    두 개의 숫자열 풀이

--- Weekly/Week_29/SWEA/두개의숫자열_주희.java
+++ Weekly/Week_29/SWEA/두개의숫자열_주희.java
@@ -1,4 +1,59 @@
 package SWEA;
 
-public class 두개의숫자열_주희 {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class 두개의숫자열_주희
+{
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+            List<Integer> A = new ArrayList<>();
+            List<Integer> B = new ArrayList<>();
+
+            for (int i = 0; i < N; i++) {
+                int temp = sc.nextInt();
+                A.add(temp);
+            }
+            for (int i = 0; i < M; i++) {
+                int temp = sc.nextInt();
+                B.add(temp);
+            }
+
+            int sum = 0;
+            if (N > M) {
+                sum = solution(M, N, B, A);
+            } else {
+                sum  = solution(N, M, A, B);
+            }
+
+            System.out.println("#" + test_case + " " + sum);
+
+        }
+    }
+
+    public static int solution(int N, int M, List<Integer> A, List<Integer> B) {
+        int sum = 0;
+        for (int i = 0; i < M-N+1; i++) {
+            int temp = 0;
+            for (int j = 0; j < N; j++) {
+                temp += A.get(j) * B.get(i+j);
+            }
+            if (temp > sum) {
+                sum = temp;
+            }
+        }
+        return sum;
+    }
+
+
 }
