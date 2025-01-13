fd0e9aa901d59d2b3e431a8648f02559da060cd6
kimju-hee <lsj240606@naver.com>
2025-01-13 15:25:20 +0900

    기능 개발 풀이

---
+++ Juhee_Java/Programmers/기능개발.java
@@ -0,0 +1,52 @@
+package Programmers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public static int[] solution(int[] progresses, int[] speeds) {
+        List<Integer> list = new ArrayList<>();
+        int day = 0;
+
+        for (int i = 0; i < progresses.length; i++) {
+            if (i == 0) {
+                int temp = 100 - progresses[i];
+                if (temp % speeds[i] == 0) {
+                    day += temp / speeds[i];
+                } else {
+                    day += temp / speeds[i] + 1;
+                }
+                list.add(1);
+            } else {
+                int temp = 100 - progresses[i];
+                int calculate = 0;
+                if (temp % speeds[i] != 0) {
+                    calculate = temp / speeds[i] + 1;
+                } else {
+                    calculate = temp / speeds[i];
+                }
+
+                if (calculate <= day) {
+                    int t = list.remove(list.size()-1);
+                    list.add(t+1);
+                } else {
+                    day = calculate;
+                    list.add(1);
+                }
+            }
+
+        }
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+
+    public static void main(String args[]) {
+        Solution solution = new Solution();
+        int[] input = {93, 30, 55};
+        int[] speed = {1, 30, 5};
+
+        int[] result = solution(input, speed);
+        for (int r: result) {
+            System.out.println(r);
+        }
+    }
+}
