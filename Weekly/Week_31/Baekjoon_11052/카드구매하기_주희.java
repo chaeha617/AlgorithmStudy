00bea8340ae60c9fceb28b2da842d6cbd3f04343
kimju-hee <lsj240606@naver.com>
2025-01-24 12:40:04 +0900

    카드구매하기 풀이

--- Weekly/Week_31/Baekjoon_11052/카드구매하기_주희.java
+++ Weekly/Week_31/Baekjoon_11052/카드구매하기_주희.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Baekjoon_11052 {
+public class 카드구매하기_주희 {
     public static void main(String args[]) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
